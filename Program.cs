eb35efab48ed4c0830bc3e3ad421db6e26f91652
Ahmer Malik <ahmer9800@gmail.com>
2019-10-01 23:01:23 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _30.cSharpHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
