f5fcf235472563e8abf67d3078d571ac2634fdbc
Ahmer Malik <ahmer9800@gmail.com>
2019-10-21 00:45:49 -0500

    c sharp notes

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ namespace HelloWorld
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args)  //this is the method name
         {
             MyClass c1 = new MyClass();
             Console.WriteLine($"Hello World! {c1.ReturnMessage()}");
