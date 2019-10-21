eeff751761a7c65f1f1bcd48c15dee21b7c67d81
Ahmer Malik <ahmer9800@gmail.com>
2019-10-21 00:52:45 -0500

    learn to pass args in console

--- Program.cs
+++ Program.cs
@@ -5,9 +5,16 @@ namespace HelloWorld
     class Program
     {
         static void Main(string[] args)  //this is the method name
+        //name of parameter is 'args'
+        //type of this param is a string array 'string[]'
         {
             MyClass c1 = new MyClass();
             Console.WriteLine($"Hello World! {c1.ReturnMessage()}");
+            Console.WriteLine("Hello" + args[0] + "!");
+            Console.WriteLine($"Hello, {args[0]} !");
         }
+        // in order to run this project in console write
+        // ' dotnet run Ahmer' and it'll pass Ahmer as the parameter.
+        // if you write dotnet run --Ahmer, it will look for a project named Ahmer
     }
 }
