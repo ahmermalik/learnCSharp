acaf316ebd10b55c17b696bab384c9e9cef4f557
Ahmer Malik <ahmer9800@gmail.com>
2019-10-22 17:53:45 -0500

    learn if statements in c#

--- Program.cs
+++ Program.cs
@@ -7,12 +7,21 @@ namespace HelloWorld
         static void Main(string[] args)  //this is the method name
         //name of parameter is 'args'
         //type of this param is a string array 'string[]'
+        
+        
         {
+            if (args.Length > 0 )
+            {
             MyClass c1 = new MyClass();
             Console.WriteLine($"Hello World! {c1.ReturnMessage()}");
-            Console.WriteLine("Hello" + args[0] + "!");
+            Console.WriteLine("Hello " + args[0] + "!");
             Console.WriteLine($"Hello, {args[0]} !");
-        }
+       
+            }
+            else{
+                Console.WriteLine("There's nothing available.");
+            }
+             }
         // in order to run this project in console write
         // ' dotnet run Ahmer' and it'll pass Ahmer as the parameter.
         // if you write dotnet run --Ahmer, it will look for a project named Ahmer
